0caa45bd9b9a73257a0d74a8c4d1f3d6258080ba
hugogdc <hugo.decolombe@gmail.com>
2022-10-28 14:52:04 -0700

    register working

--- database/seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `frenchiedb`.`lvlscore` (`idlvlscore`, `modulenumber`, `lvlnumber`, `score`) VALUES ('1', '1', '1', '0');
-
-INSERT INTO `frenchiedb`.`modulescore` (`idmodulescore`, `idlvlscore`) VALUES ('1', '1');
-
-INSERT INTO `frenchiedb`.`user` (`iduser`, `pseudo`, `email`, `password`, `idmodulescore`) VALUES ('1', 'hugo', 'hugo@gmail.com', 'password', '1');
